74abe30b0451fa00e31dcab31ff97494aa3d7335
Supsub121 <34673329+Supsub121@users.noreply.github.com>
2018-05-07 23:53:26 +0530

    Add files via upload

---
+++ 6b.pl
@@ -0,0 +1,28 @@
+#! /usr/bin/perl -wT
+
+use CGI':standard';
+
+print "Enter 1st number\n";
+chomp($a = <STDIN>);
+print "Enter 2nd number\n";
+chomp($b = <STDIN>);
+print "Enter 3rd number\n";
+chomp($c = <STDIN>);
+
+findbig($a,$b,$c);
+
+
+sub findbig{
+$big = 0;
+foreach $x (@_){
+    if($big<$x)
+    {
+        $big = $x;
+     }
+}
+
+print "Biggest :$big";
+
+}
+
+
